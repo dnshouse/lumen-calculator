277fe42732d5cc3efd34a9e52c5623d098e15e7b
Deyan <dnsheadstate@gmail.com>
2022-02-21 00:27:07 +0000

    Calculator endpoint

--- tests/Http/Controllers/CalculatorControllerTest.php
+++ tests/Http/Controllers/CalculatorControllerTest.php
@@ -11,24 +11,15 @@ class CalculatorControllerTest extends TestCase
      */
     public function test_plus_calculate_response()
     {
-        /**
-         * Given
-         */
         $requestData = [
             'a' => 56,
             'b' => 2,
             'operation' => '+'
         ];
 
-        /**
-         * When
-         */
         $this->post('/api/v1/calculate', $requestData);
         $response = json_decode($this->response->getContent(), true);
 
-        /**
-         * Then
-         */
         $this->assertResponseOk();
         $this->assertEquals(56+2, $response['result']);
     }
@@ -38,24 +29,15 @@ class CalculatorControllerTest extends TestCase
      */
     public function test_minus_calculate_response()
     {
-        /**
-         * Given
-         */
         $requestData = [
             'a' => 56,
             'b' => 2,
             'operation' => '-'
         ];
 
-        /**
-         * When
-         */
         $this->post('/api/v1/calculate', $requestData);
         $response = json_decode($this->response->getContent(), true);
 
-        /**
-         * Then
-         */
         $this->assertResponseOk();
         $this->assertEquals(56-2, $response['result']);
     }
@@ -65,24 +47,15 @@ class CalculatorControllerTest extends TestCase
      */
     public function test_multiplication_calculate_response()
     {
-        /**
-         * Given
-         */
         $requestData = [
             'a' => 56,
             'b' => 2,
             'operation' => '*'
         ];
 
-        /**
-         * When
-         */
         $this->post('/api/v1/calculate', $requestData);
         $response = json_decode($this->response->getContent(), true);
 
-        /**
-         * Then
-         */
         $this->assertResponseOk();
         $this->assertEquals(56*2, $response['result']);
     }
@@ -92,24 +65,15 @@ class CalculatorControllerTest extends TestCase
      */
     public function test_division_calculate_response()
     {
-        /**
-         * Given
-         */
         $requestData = [
             'a' => 56,
             'b' => 2,
             'operation' => '/'
         ];
 
-        /**
-         * When
-         */
         $this->post('/api/v1/calculate', $requestData);
         $response = json_decode($this->response->getContent(), true);
 
-        /**
-         * Then
-         */
         $this->assertResponseOk();
         $this->assertEquals(56/2, $response['result']);
     }
@@ -119,23 +83,14 @@ class CalculatorControllerTest extends TestCase
      */
     public function test_division_by_zero_calculate_response()
     {
-        /**
-         * Given
-         */
         $requestData = [
             'a' => 56,
             'b' => 0,
             'operation' => '/'
         ];
 
-        /**
-         * When
-         */
         $this->post('/api/v1/calculate', $requestData);
 
-        /**
-         * Then
-         */
         $this->assertResponseStatus(422);
     }
 
@@ -144,23 +99,14 @@ class CalculatorControllerTest extends TestCase
      */
     public function test_unknown_operation_calculate_response()
     {
-        /**
-         * Given
-         */
         $requestData = [
             'a' => 56,
             'b' => 0,
             'operation' => '.'
         ];
 
-        /**
-         * When
-         */
         $this->post('/api/v1/calculate', $requestData);
 
-        /**
-         * Then
-         */
         $this->assertResponseStatus(422);
     }
 }
